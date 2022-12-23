ed4fcfd792088f84deb2941fbec48f16fd39ad74
ASWIN K MANOJ <aswinkmanoj101@gmail.com>
2022-12-23 12:55:14 +0530

    iterables

---
+++ Week-2/iterable.js
@@ -0,0 +1,40 @@
+var veggies = ["onion", "parsley", "carrot"];
+
+console.log(veggies.length); //Returns the numbers  of element in the array
+
+console.log(veggies[0]); //Returns the first element in the array
+console.log(veggies[1]); //Returns the second element in the array
+
+for (var i = 0; i < veggies.length; i++) {
+    console.log(veggies[i]);
+}
+// Logs out the elements in the array one by one
+
+// Iterating through the string
+
+var a = "HelloWorld!";
+
+console.log(a);
+
+console.log(a[0]); // Returns the first element of the string
+console.log(a[1]); // Returns the second element of the string
+
+for (var i = 0; i < a.length; i++) {
+    console.log(a[i]);
+}
+// log out the letters of the string one by one
+
+// You can iterate through string and array in the same way but the array and string will never be the same
+
+// Example
+
+// console.log(a.pop()) // Shows an error cause the string doesn't have pop method
+
+var h = "Hello";
+var i = "World";
+
+console.log(h + i);
+
+var g = ["jo", "lol"]
+
+console.log(veggies + g) // appends the elements of g array into veggies array
